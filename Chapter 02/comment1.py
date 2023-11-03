b2c19504b0dc7388e5c34b0193542aae9f37b0c0
rainer.telesko <rainer.telesko@gmail.com>
2023-11-03 10:19:36 +0100

    Update comment1.py

--- Chapter 02/comment1.py
+++ Chapter 02/comment1.py
@@ -1,5 +1,24 @@
-# This program displays a person's
-# name and address.
+# Single-line comment This program displays a person's name and address.
 print('Kate Austen')
 print('123 Full Circle Drive')
 print('Asheville, NC 28899')
+
+"""
+This is a multi-line comment with docstrings
+
+print("Hello world")
+print("Hello universe")
+print("Hello everyone")
+"""
+
+print("Hello campers")
+
+# In Python, a backslash (\) is a line continuation character.
+# If a backslash is placed at the end of a line,
+# the line is considered to continue on the next line.
+
+n = 1 + 2 \
+    + 3
+
+print(n)
+# 6
