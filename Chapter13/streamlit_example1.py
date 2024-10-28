3a3a96087c6df60048bf4aa514dec1d29229e7cb
rainer Telesko <rainer.telesko@bluewin.ch>
2024-10-28 16:55:44 +0100

    Changes: Adding streamlit, with-statement, json examples

---
+++ Chapter13/streamlit_example1.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import streamlit as st
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import streamlit as st
+
+# Selectbox for bins
+option = st.selectbox(
+    "Select number of bins",
+    (10, 20, 30, 40, 50, 60, 70, 80),
+)
+
+# Button triggers the histogram generation
+if st.button("Show histogram"):
+    # Generate random data
+    arr = np.random.normal(1, 1, size=100)
+
+    # Plotting
+    fig, ax = plt.subplots()
+    ax.hist(arr, bins=option)
+    st.pyplot(fig)
+
